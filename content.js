4c706db605ad6601bae438c1071a10427e9e3944
vectorvr10 <86861836+Vectorvr10@users.noreply.github.com>
2026-01-03 09:34:16 -0300

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-console.log('Extractor de Laboratorio cargado');
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'getPageText') {
-    sendResponse({ text: document.body.innerText });
-  }
-  return true;
-});
